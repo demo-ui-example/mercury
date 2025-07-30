97b116a964def312f3436ea01610b38eed25a63c
Gary <hai.alphacreative@gmail.com>
2025-07-30 15:35:24 +0700

    change style swiper facility

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -101,7 +101,7 @@ function bookingForm() {
         calendar.style.top = rect.bottom + window.scrollY + "px";
         calendar.style.left = rect.left + window.scrollX + "px";
       }
-    }
+    },
   });
 
   // Counter functionality
@@ -164,12 +164,12 @@ function sectionAccommodation() {
       slidesOffsetAfter: spaceafter,
       pagination: {
         el: $pagination[0],
-        type: "progressbar"
+        type: "progressbar",
       },
       navigation: {
         prevEl: $prev[0],
-        nextEl: $next[0]
-      }
+        nextEl: $next[0],
+      },
     });
   });
 }
@@ -182,12 +182,10 @@ function swiperFacility() {
       watchSlidesProgress: true,
       speed: 1500,
       loop: true,
-      autoplay: {
-        delay: 3000
-      },
+
       pagination: {
         el: el.querySelector(".swiper-pagination"),
-        clickable: true
+        clickable: true,
         // renderBullet: function (index, className) {
         //   return `
         //     <button class="${className}">
@@ -197,7 +195,7 @@ function swiperFacility() {
       },
       navigation: {
         nextEl: el.querySelector(".swiper-button-next"),
-        prevEl: el.querySelector(".swiper-button-prev")
+        prevEl: el.querySelector(".swiper-button-prev"),
       },
       on: {
         init(swiper) {
@@ -272,8 +270,8 @@ function swiperFacility() {
               slideInner.style.transition = `${speed}ms ${easing}`;
             }
           });
-        }
-      }
+        },
+      },
     });
   });
 }
@@ -309,12 +307,12 @@ function swiperAccommodation() {
       parallax: true,
       pagination: {
         el: $pagination[0],
-        type: "progressbar"
+        type: "progressbar",
       },
       navigation: {
         prevEl: $prev[0],
-        nextEl: $next[0]
-      }
+        nextEl: $next[0],
+      },
     });
 
     // Handle modal gallery slider
@@ -346,17 +344,17 @@ function swiperAccommodation() {
         // centeredSlides: true,
         pagination: {
           el: $paginationG[0],
-          type: "progressbar"
+          type: "progressbar",
         },
         navigation: {
           prevEl: $prevG[0],
-          nextEl: $nextG[0]
+          nextEl: $nextG[0],
         },
         breakpoints: {
           991: {
             spaceBetween: 40,
-            slidesPerView: "auto"
-          }
+            slidesPerView: "auto",
+          },
         },
         on: {
           slideChange: function () {
@@ -368,8 +366,8 @@ function swiperAccommodation() {
           init: function () {
             // Reveal Swiper after initialization
             $gallery.removeClass("swiper-hidden").addClass("swiper-visible");
-          }
-        }
+          },
+        },
       });
     }
   });
@@ -384,7 +382,7 @@ function ctaMess() {
       self.direction === 1
         ? $("#cta-mess").addClass("hide")
         : $("#cta-mess").removeClass("hide");
-    }
+    },
   });
 }
 function distortionImg() {
@@ -405,7 +403,7 @@ function distortionImg() {
         angle: 0,
         image1: imageSrc,
         image2: imageSrc,
-        displacementImage: imgReplace
+        displacementImage: imgReplace,
       });
     }
   });
@@ -459,16 +457,16 @@ function loadingBanner() {
         ScrollTrigger.refresh(); // Update lại ScrollTrigger
 
         window.scrollTo({ top: 0, behavior: "smooth" }); // Cuộn về đầu trang
-      }
-    }
+      },
+    },
   });
 
   tl.to(".anim-clip-circle", {
-    clipPath: "circle(70.7% at 50% 50%)"
+    clipPath: "circle(70.7% at 50% 50%)",
   }).to(
     ".banner-container img",
     {
-      scale: 1
+      scale: 1,
     },
     0
   );
@@ -537,7 +535,7 @@ function magicCursor() {
     speed: 0.8,
     scale: 1,
     skewing: 0,
-    skewingText: 0
+    skewingText: 0,
   });
 
   const element = document.querySelectorAll("[data-cursor]");
@@ -585,7 +583,7 @@ function distortionImgNav() {
           angle: 0,
           image1: imageSrc,
           image2: imageSrc,
-          displacementImage: imgReplace
+          displacementImage: imgReplace,
         });
         wrapper.__hoverEffect = effectInstance;
       }
@@ -641,7 +639,7 @@ function bookingOffer() {
         } catch (error) {
           console.error("Lỗi trong Lightpick onSelect:", error);
         }
-      }
+      },
     });
   }
 
@@ -657,7 +655,7 @@ function bookingOffer() {
       name: form.find("input[name='name']"),
       phone: form.find("input[name='phone']"),
       email: form.find("input[name='email']"),
-      offer_id: form.find("input[name='offer_id']")
+      offer_id: form.find("input[name='offer_id']"),
     };
 
     form.find(".error-message").remove();
@@ -688,7 +686,7 @@ function bookingOffer() {
         name: fields.name.val().trim(),
         phone: fields.phone.val().trim(),
         email: fields.email.val().trim(),
-        offer_id: fields.offer_id.val().trim()
+        offer_id: fields.offer_id.val().trim(),
       },
       beforeSend: function () {
         form.find("button[type='submit']").addClass("aloading");
@@ -713,7 +711,7 @@ function bookingOffer() {
       },
       error: function (xhr, status, error) {
         console.error("Lỗi khi gửi form:", error);
-      }
+      },
     });
   });
 }
@@ -724,14 +722,14 @@ function animationText() {
     const splitDescription = new SplitText(description, {
       type: "lines",
       linesClass: "line",
-      mask: "lines"
+      mask: "lines",
     });
 
     gsap.fromTo(
       splitDescription.lines,
       {
         yPercent: 100,
-        willChange: "transform"
+        willChange: "transform",
       },
       {
         yPercent: 0,
@@ -741,9 +739,9 @@ function animationText() {
 
         scrollTrigger: {
           trigger: description,
-          start: "top 60%"
+          start: "top 60%",
           // markers: true,
-        }
+        },
       }
     );
   });
@@ -754,18 +752,18 @@ function animationText() {
       {
         "will-change": "opacity, transform",
         opacity: 0,
-        y: 20
+        y: 20,
       },
       {
         scrollTrigger: {
           trigger: element,
           start: "top 70%",
-          end: "bottom 70%"
+          end: "bottom 70%",
         },
         opacity: 1,
         y: 0,
         duration: 0.5,
-        ease: "sine.out"
+        ease: "sine.out",
       }
     );
   });
@@ -790,9 +788,9 @@ function itemParallax() {
           end: "bottom top",
           scrub: 1,
           ease: "power4",
-          delay: 0.2
+          delay: 0.2,
           // markers: true
-        }
+        },
       }
     );
   });
@@ -811,8 +809,8 @@ function itemParallax() {
         trigger: section,
         start: "top 80%",
         end: "bottom top",
-        scrub: true
-      }
+        scrub: true,
+      },
     });
   });
 }
@@ -838,7 +836,7 @@ function bookingFormMobile() {
         calendar.style.top = rect.bottom + window.scrollY + "px";
         calendar.style.left = rect.left + window.scrollX + "px";
       }
-    }
+    },
   });
 
   // Counter functionality
@@ -916,7 +914,7 @@ function loading() {
   gsap.set(".effect-line-banner", { opacity: 0 });
 
   const tl = gsap.timeline({
-    defaults: { duration: 2, ease: "power2.inOut" }
+    defaults: { duration: 2, ease: "power2.inOut" },
   });
 
   gsap.delayedCall(1, () => {
@@ -924,7 +922,7 @@ function loading() {
       const splitDescriptionBanner = new SplitText(description, {
         type: "lines",
         linesClass: "line",
-        mask: "lines"
+        mask: "lines",
       });
 
       gsap.fromTo(
@@ -932,7 +930,7 @@ function loading() {
         {
           yPercent: 100,
           willChange: "transform",
-          opacity: 0
+          opacity: 0,
         },
         {
           yPercent: 0,
@@ -943,7 +941,7 @@ function loading() {
           onStart: () => {
             // Hiện container khi animation bắt đầu
             gsap.set(description, { opacity: 1 });
-          }
+          },
         }
       );
     });
@@ -956,7 +954,7 @@ function loading() {
       clipPath: "inset(0% 0% 100% 0%)",
       onComplete: () => {
         document.querySelector(".loading").classList.add("d-none");
-      }
+      },
     }
   );
 }
@@ -1006,7 +1004,7 @@ function pageOffer() {
       setTimeout(function () {
         $("html, body").animate(
           {
-            scrollTop: $target.offset().top - 80
+            scrollTop: $target.offset().top - 80,
           },
           600
         ); // Thời gian scroll
